113d8040dafa804514ffe00b74dee6aacf3e12ac
dp_chua <dp_chua@hotmail.com>
2017-03-02 11:25:14 -0500

    home partials working

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-angular.module('movie-angular-app', []);
+angular.module('movie-angular-app', ['ui.router'])
+    .config(movieRouter);
+
+    // movieRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function movieRouter($stateProvider, $urlRouterProvider) {
+
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+        .state('homepage', {
+          url: '/',
+          templateUrl: "/partials/home.html"
+        });
+      }

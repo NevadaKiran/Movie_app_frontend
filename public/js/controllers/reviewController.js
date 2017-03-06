aae8b3f515ee095b4a09d9a0cd856e03455a5a9e
KiranWork <kiran8654@yahoo.com>
2017-03-05 23:10:21 -0800

    review controller still doesn't work

--- public/js/controllers/reviewController.js
+++ public/js/controllers/reviewController.js
@@ -4,11 +4,11 @@ function ReviewController($scope, $http, $state, $stateParams, $rootScope){
   var server = 'http://localhost:3000'
   self.review = [];
 
-  $rootScope.$on('fetchData', function(event, data){
-    console.log(data)
-    self.user = data.id
-    populateInitialState(data)
-  });
+  // $rootScope.$on('fetchData', function(event, data){
+  //   console.log(data)
+  //   self.user = data.id
+  //   populateInitialState(data)
+  // });
 
   function createReview(currentUser1, newReview){
     console.log(self.newReview);
@@ -26,30 +26,30 @@ function ReviewController($scope, $http, $state, $stateParams, $rootScope){
       });
   }
 
-  function deleteReview(id, currentUser1){
-    currentUser1 = $rootScope.currentUser.id
-    console.log(id)
-    $http.delete(`${server}/reviews/${id}`)
-    .then(function(response){
-      self.savedReviews = response.data.currentUser.reviews
-    })
-  }
-
-  function updateReview(newReview){
-
-    console.log(self.newMovie);
-    currentUser1 = $rootScope.currentUser.id
-    console.log(currentUser1)
-
-    $http.patch(`${server}/reviews/${id}`)
-      .then(function(response){
-        console.log(response.data)
-        self.movies.push(response.data);
-        newMovie = {};
-        console.log("I CAN HAZ MOVIREVIEW?!!", newReview);
-
-        $state.go('usershow', {userId: currentUser1.id})
-      });
-  }
-
+  // function deleteReview(id, currentUser1){
+  //   currentUser1 = $rootScope.currentUser.id
+  //   console.log(id)
+  //   $http.delete(`${server}/reviews/${id}`)
+  //   .then(function(response){
+  //     self.savedReviews = response.data.currentUser.reviews
+  //   })
+  // }
+  //
+  // function updateReview(newReview){
+  //
+  //   console.log(self.newMovie);
+  //   currentUser1 = $rootScope.currentUser.id
+  //   console.log(currentUser1)
+  //
+  //   $http.patch(`${server}/reviews/${id}`)
+  //     .then(function(response){
+  //       console.log(response.data)
+  //       self.movies.push(response.data);
+  //       newMovie = {};
+  //       console.log("I CAN HAZ MOVIREVIEW?!!", newReview);
+  //
+  //       $state.go('usershow', {userId: currentUser1.id})
+  //     });
+  // }
+  //
 }
